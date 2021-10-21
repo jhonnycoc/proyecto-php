28a51b8b74f71c4281390f07ad4b868d263ac625
Jhonny <jhonnyalonso2358@gmail.com>
2021-10-21 18:33:39 -0500

    cambio nombre de desarrollador

--- includes/pie.php
+++ includes/pie.php
@@ -4,7 +4,7 @@
 
 		<!-- PIE DE PÁGINA -->
 		<footer id="pie">
-			<p>Desarrollado por Víctor Robles &copy; 2018</p>
+			<p>Desarrollado por Jhonny Alonso &copy; 2021</p>
 		</footer>
 		
 	</body>
